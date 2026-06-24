b2b45e260f43a1da65759ae34fcc4339bdf08360
Paulo Andrade <paulinhoboneti@yahoo.com.br>
2026-06-24 15:36:35 -0300

    Add enemy defeat condition for locked door mechanism

--- scenes/LockedDoor/locked_door.gd
+++ scenes/LockedDoor/locked_door.gd
@@ -1,8 +1,11 @@
 extends StaticBody2D
 
 @export var buttons_needed: int = 1
+@export var enemies_needed: int = 0  # 0 = enemy trigger disabled
 
 var _tracked_buttons: Array[Area2D] = []
+var _enemies_defeated: int = 0
+var _opened_by_enemies: bool = false
 
 func _on_puzzle_button_pressed(button: Area2D) -> void:
 	_register_button(button)
@@ -10,6 +13,12 @@ func _on_puzzle_button_pressed(button: Area2D) -> void:
 
 func _on_puzzle_button_unpressed(button: Area2D) -> void:
 	_register_button(button)
+	_update_door_state()
+
+func _on_enemy_died() -> void:
+	_enemies_defeated += 1
+	if enemies_needed > 0 and _enemies_defeated >= enemies_needed:
+		_opened_by_enemies = true
 	_update_door_state()
 
 func _register_button(button: Area2D) -> void:
@@ -23,6 +32,7 @@ func _update_door_state() -> void:
 		if is_instance_valid(button) and button.is_pressed():
 			pressed_count += 1
 
-	var is_open := pressed_count >= buttons_needed
+	var buttons_open := buttons_needed > 0 and pressed_count >= buttons_needed
+	var is_open := buttons_open or _opened_by_enemies
 	visible = not is_open
 	$CollisionShape2D.set_deferred("disabled", is_open)
